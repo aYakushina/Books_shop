401f4ac2020de7a3278df1f3265691600231c245
Alyona <a.yakushina@primelabs.ru>
2011-01-18 21:42:11 +0300

    change show books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+	default_scope :order => 'title'
 	validates :autor, :presence => true 
 	validates_format_of :autor, :with => /\A[a-zA-Zа-яёА-ЯЁ\s\.]+\z/, :message => "Используйте только буквы, '.' и пробел" 
 	validates_length_of :autor, :maximum => 50
